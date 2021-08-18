392e31915b462ca05c8730c1d1a5bf31fdb75f79
lady_c <charlier.chloe@outlook.fr>
2021-08-18 15:13:37 +0100

    working on a visitor counter

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-window.location.hash = "entry";
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch('https://api.countapi.xyz/update/perryfeinstein.com/8886648850?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+        });
+}
